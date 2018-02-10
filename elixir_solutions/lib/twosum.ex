24ccc122918bc947bb8f00c00356fba56d7490bb
Hugo Roque <hugolnx@gmail.com>
2018-02-10 12:09:49 -0200

    Formatting twosum solution

--- elixir_solutions/lib/twosum.ex
+++ elixir_solutions/lib/twosum.ex
@@ -2,13 +2,16 @@
 
 defmodule Twosum do
   def solve(target, list, existent_indexes \\ {0, %{}})
+
   def solve(_, [], _) do
     {nil, nil}
   end
+
   def solve(target, [head | tail], {i, existent}) do
-    existent_inx = existent[target-head]
+    existent_inx = existent[target - head]
+
     if is_nil(existent_inx) do
-      solve(target, tail, {i+1, Map.put(existent, head, i)})
+      solve(target, tail, {i + 1, Map.put(existent, head, i)})
     else
       {existent_inx, i}
     end
