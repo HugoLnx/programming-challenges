4785883bcb3466b128cd7a66c9d6f3ee362c408e
Hugo Roque <hugolnx@gmail.com>
2018-02-10 18:18:39 -0200

    Using a Dequeue on Sliding Window Maximum solution

--- elixir_solutions/lib/sliding_window_maximum.ex
+++ elixir_solutions/lib/sliding_window_maximum.ex
@@ -1,11 +1,12 @@
 defmodule SlidingWindowMaximum do
+  @empty_queue {[], []}
   import Enum
 
   def maximum(tuple, window_size) when tuple_size(tuple) < window_size,
     do: {:error, :window_is_too_big}
 
   def maximum(tuple, window_size) do
-    {:ok, get_maximum(tuple, window_size, [], 0, [])}
+    {:ok, get_maximum(tuple, window_size, :queue.new(), 0, [])}
   end
 
   defp get_maximum(tuple, _, _, inx, output) when inx >= tuple_size(tuple),
@@ -17,32 +18,36 @@ defmodule SlidingWindowMaximum do
       |> remove_useless_inxs(tuple, elem(tuple, inx))
       |> remove_out_of_window_inxs(window_size, inx)
 
-    userful_inxs = [inx | userful_inxs]
+    userful_inxs = :queue.in(inx, userful_inxs)
 
     output = next_output(tuple, window_size, userful_inxs, inx, output)
     get_maximum(tuple, window_size, userful_inxs, inx + 1, output)
   end
 
-  defp remove_useless_inxs([], _, _), do: []
+  defp remove_useless_inxs(@empty_queue = userful_inxs, _, _), do: userful_inxs
 
-  defp remove_useless_inxs([head | tail] = userful_inxs, tuple, value)
-       when elem(tuple, head) < value,
-       do: remove_useless_inxs(tail, tuple, value)
+  defp remove_useless_inxs(userful_inxs, tuple, value) do
+    {{:value, head}, tail} = :queue.out_r(userful_inxs)
 
-  defp remove_useless_inxs([head | tail] = userful_inxs, _tuple, _value), do: userful_inxs
+    if elem(tuple, head) < value,
+      do: remove_useless_inxs(tail, tuple, value),
+      else: userful_inxs
+  end
 
-  defp remove_out_of_window_inxs([], _, _), do: []
+  defp remove_out_of_window_inxs(@empty_queue = userful_inxs, _, _), do: userful_inxs
 
-  defp remove_out_of_window_inxs([head | tail] = userful_inxs, window_size, inx)
-       when head <= inx - window_size,
-       do: remove_out_of_window_inxs(tail, window_size, inx)
+  defp remove_out_of_window_inxs(userful_inxs, window_size, inx) do
+    {{:value, head}, tail} = :queue.out_r(userful_inxs)
 
-  defp remove_out_of_window_inxs(userful_inxs, window_size, inx), do: userful_inxs
+    if head <= inx - window_size,
+      do: remove_out_of_window_inxs(tail, window_size, inx),
+      else: userful_inxs
+  end
 
-  defp next_output(_tuple, window_size, _userful_inxs, inx, _output) when inx < window_size - 1,
+  defp next_output(_, window_size, _, inx, _) when inx < window_size - 1,
     do: []
 
-  defp next_output(tuple, _window_size, userful_inxs, _inx, output) do
-    [elem(tuple, List.last(userful_inxs)) | output]
+  defp next_output(tuple, _, userful_inxs, _, output) do
+    [elem(tuple, :queue.peek(userful_inxs) |> elem(1)) | output]
   end
 end
