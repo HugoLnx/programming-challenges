1def584fb73e4187c9d9a5035e35007102fb1202
Hugo Roque <hugolnx@gmail.com>
2018-02-11 09:25:27 -0200

    Different solution to search in a rotated array

--- elixir_solutions/lib/search_rotated_array.ex
+++ elixir_solutions/lib/search_rotated_array.ex
@@ -1,25 +1,30 @@
 # https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+# https://www.youtube.com/watch?v=uufaK2uLnSI&t=250s
 defmodule SearchRotatedArray do
-  def search(tuple, e) do
-    r = rotations(tuple)
-    binary_search(tuple, e, r, 0, tuple_size(tuple) - 1)
-  end
+  def search(tuple, target, i \\ nil, j \\ nil)
+  def search(tuple, target, nil, nil), do: search(tuple, target, 0, tuple_size(tuple) - 1)
+  def search(tuple, target, i, j) when j < i, do: {:error, :not_found}
 
-  defp binary_search({}, _, _, _, _), do: {:error, :not_found}
-
-  defp binary_search(tuple, e, r, i, j) do
+  def search(tuple, target, i, j) do
     k = (i + j) |> div(2)
 
     cond do
-      j < i -> {:error, :not_found}
-      e == relem(tuple, r, k) -> {:ok, (r + k) |> rem(tuple_size(tuple))}
-      e > relem(tuple, r, k) -> binary_search(tuple, e, r, k + 1, j)
-      true -> binary_search(tuple, e, r, i, k - 1)
-    end
-  end
+      elem(tuple, k) == target ->
+        {:ok, k}
 
-  defp relem(tuple, r, inx) do
-    tuple |> elem((r + inx) |> rem(tuple_size(tuple)))
+      elem(tuple, i) < elem(tuple, k) ->
+        if elem(tuple, i) <= target and target < elem(tuple, k),
+          do: search(tuple, target, i, k - 1),
+          else: search(tuple, target, k + 1, j)
+
+      elem(tuple, j) > elem(tuple, k) ->
+        if elem(tuple, k) < target and target <= elem(tuple, j),
+          do: search(tuple, target, k + 1, j),
+          else: search(tuple, target, i, k - 1)
+
+      true ->
+        {:error, :not_found}
+    end
   end
 
   defguard found_break(tuple, i, j)
